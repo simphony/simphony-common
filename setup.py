5db9618d2d428a8d54ffd35b1e360546d7a37aaf
Ioannis Tziakos <ioannist@enthought.com>
2014-08-08 00:19:27 +0100

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as readme:
+    README_TEXT = readme.read()
+
+VERSION = '0.0.1dev'
+
+
+def write_version_py(filename=None):
+    if filename is None:
+        filename = os.path.join(
+            os.path.dirname(__file__), 'pikos', 'version.py')
+    ver = """\
+version = '%s'
+"""
+    fh = open(filename, 'wb')
+    try:
+        fh.write(ver % VERSION)
+    finally:
+        fh.close()
+
+
+write_version_py()
+
+setup(
+    name='simphony',
+    version=VERSION,
+    author='SimPhoNy FP7 European Project',
+    description='The native implementation of the SimPhoNy cuds objects',
+    long_description=README_TEXT,
+    packages=find_packages())
