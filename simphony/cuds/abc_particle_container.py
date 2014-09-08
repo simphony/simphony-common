64b5c3454952ea7f05df550e5d07bfb0291d6885
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2014-09-08 12:53:38 +0200

    removing returns

--- simphony/cuds/abc_particle_container.py
+++ simphony/cuds/abc_particle_container.py
@@ -10,49 +10,39 @@ class ABCParticleContainer(object):
     @abc.abstractmethod
     def add_particle(p):
         """Add particle"""
-        return
 
     @abc.abstractmethod
     def update_particle(p):
         """Update particle"""
-        return
 
     @abc.abstractmethod
     def get_particle(id):
         """Get particle"""
-        return
 
     @abc.abstractmethod
     def remove_particle(id):
         """Remove particle"""
-        return
 
     @abc.abstractmethod
     def iter_particles(ids):
         """iter particles"""
-        return
 
     @abc.abstractmethod
     def add_bond(b):
         """Add bond"""
-        return
 
     @abc.abstractmethod
     def update_bond(b):
         """Update bond"""
-        return
 
     @abc.abstractmethod
     def get_bond(id):
         """Get bond"""
-        return
 
     @abc.abstractmethod
     def remove_bond(id):
         """Remove bond"""
-        return
 
     @abc.abstractmethod
     def iter_bonds(ids):
         """iter bonds"""
-        return
