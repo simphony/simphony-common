89dfd694fbdb6ec4dd3ba5720f3d770e00d6c1d1
Ioannis Tziakos <ioannist@enthought.com>
2014-09-19 19:24:35 +0100

    rework file benchmarking

--- simphony/bench/cuds_file_bench.py
+++ simphony/bench/cuds_file_bench.py
@@ -1,57 +1,80 @@
 from __future__ import print_function
 
 import os
+import shutil
+import tempfile
 
 from simphony.bench.util import bench
 from simphony.io.cuds_file import CudsFile
 from simphony.cuds.particle import Particle
 
+particles = [
+    Particle(coordinates=(0.0, 1.1, 2.2)) for i in range(1000)]
+
+id_particles = [
+    Particle(id=i, coordinates=(0.0, 1.1, 2.2)) for i in range(1000)]
 
 def create_file_with_particles():
-    file = CudsFile.open('test.cuds')
-    pc = file.add_particle_container("test")
-    for i in xrange(10000):
-        pc.add_particle(Particle(id=i, coordinates=(0.0, 1.1, 2.2)))
-    file.close()
-    os.remove('test.cuds')
+    with Container() as pc:
+        add_particles_to_container(pc)
+
+def create_file_with_id_particles():
+    with Container() as pc:
+        add_id_particles_to_container(pc)
+
+def add_id_particles_to_container(particle_container):
+    for particle in id_particles:
+        particle_container.add_particle(particle)
+
+def add_particles_to_container(particle_container):
+    for particle in particles:
+        particle_container.add_particle(particle)
+
+def iter_particles_in_container(particle_container):
+    return [particle for particle in particle_container.iter_particles()]
+
+def update_coordinates_of_particles_in_container(particle_container):
+    for particle in particle_container.iter_particles():
+        particle.coordinates = (0.1, 1.0, 1.0)
+        particle_container.update_particle(particle)
 
 
-def iter_particles_in_file(particle_container):
-    id = 0
-    for p in particle_container.iter_particles():
-        id = p.id
-    return id
-
-
-def update_coordinates_of_particles_in_file_using_iter(particle_container):
-    for p in particle_container.iter_particles():
-        p.coordinates = (0.1, 1.0, 1.0)
-        particle_container.update_particle(p)
-
-
-class FilledParticleContainer(object):
+class Container(object):
     def __init__(self):
-        pass
+        self.temp_dir = tempfile.mkdtemp()
+        self._filename = os.path.join(self.temp_dir, 'test.cuds')
+        if os.path.exists(self._filename):
+            print('exists')
 
     def __enter__(self):
-        self._file = CudsFile.open('test.cuds')
+        self._file = CudsFile.open(self._filename)
         pc = self._file.add_particle_container("test")
-        for i in xrange(10000):
-            pc.add_particle(Particle(id=i, coordinates=(0.0, 1.1, 2.2)))
         return pc
 
     def __exit__(self, type, value, tb):
-        self._file.close()
-        os.remove('test.cuds')
+        if os.path.exists(self._filename):
+            self._file.close()
+        shutil.rmtree(self.temp_dir)
 
 
 if __name__ == '__main__':
-    print("create_file_with_particles:", bench(
-        lambda: create_file_with_particles()))
 
-    with FilledParticleContainer() as pc:
-        print("iter_particles_in_file", bench(
-            lambda: iter_particles_in_file(pc)))
+    print(
+         "create_file_with_particles:",
+         bench(lambda: create_file_with_particles()))
 
-        print("update_coordinates_of_particles_in_file_using_iter", bench(
-            lambda: update_coordinates_of_particles_in_file_using_iter(pc)))
+    print(
+         "create_file_with_particles:",
+         bench(lambda: create_file_with_id_particles()))
+
+    with Container() as pc:
+        add_particles_to_container(pc)
+        print(
+            "iter_particles_in_file", bench(
+            lambda: iter_particles_in_container(pc)))
+
+    with Container() as pc:
+        add_particles_to_container(pc)
+        print(
+            "update_coordinates_of_particles_in_file_using_iter",
+            bench(lambda: update_coordinates_of_particles_in_container(pc)))
