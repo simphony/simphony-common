eba922913b03580ebad996d9d3e68174fae58f54
itziakos <mail@itziakos.gr>
2015-01-21 11:29:49 +0000

    fix bench util to respect the actual time of repeats when adjust_runs is true

--- simphony/bench/util.py
+++ simphony/bench/util.py
@@ -7,12 +7,14 @@ def bench(stmt='pass', setup='pass', repeat=5, adjust_runs=True):
 
     """
     timer = Timer(stmt, setup)
-    for i in range(100):
-        number = 10**i
-        time = timer.timeit(number)
-        if time > 0.2 or not adjust_runs:
-            break
-
+    if adjust_runs:
+        for i in range(100):
+            number = 10**i
+            time = timer.timeit(number)
+            if time > 0.2:
+                break
+    else:
+        number = 1
     times = [timer.timeit(number) for i in range(repeat)]
     message = '{} calls, best of {} repeats: {:f} sec per call'
     return message.format(number, repeat, min(times)/number)
