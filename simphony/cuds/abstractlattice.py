a4c0702f0349387f86e03c32e6e9aa81c79f9141
tuopuu <tuomas.a.puurtinen@jyu.fi>
2015-01-26 15:02:27 +0200

    Added docstrings to the methods.

--- simphony/cuds/abstractlattice.py
+++ simphony/cuds/abstractlattice.py
@@ -15,18 +15,55 @@ class ABCLattice(object):
 
     @abstractmethod
     def get_node(self, index):
+        """Get a copy of the node corresponding to the given index.
+
+        Parameters:
+        -----------
+        index: tuple of D x int (node index coordinate)
+
+        Returns:
+        -----------
+        A reference to a LatticeNode object
+        """
         pass
 
     @abstractmethod
     def update_node(self, lat_node):
+        """Update the corresponding lattice node (data copied).
+
+        Parameters:
+        -----------
+        lat_node: reference to a LatticeNode object
+            data copied from the given node
+        """
         pass
 
     @abstractmethod
     def iter_nodes(self, indices=None):
+        """Get an iterator over the LatticeNodes described by the indices.
+
+        Parameters:
+        -----------
+        indices: iterable set of D x int (node index coordinates)
+
+        Returns:
+        -----------
+        A generator for LatticeNode objects
+        """
         pass
 
     @abstractmethod
     def get_coordinate(self, index):
+        """Get coordinate of the given index coordinate.
+
+        Parameters:
+        -----------
+        index: D x int (node index coordinate)
+
+        Returns:
+        -----------
+        D x float
+        """
         pass
 
 
