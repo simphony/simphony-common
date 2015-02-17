00045e81abef7613dde59d950608499bafa2fab9
Ioannis Tziakos <ioannist@enthought.com>
2015-02-17 17:59:13 +0000

    add helper functions for particles and bonds

--- simphony/io/tests/utils.py
+++ simphony/io/tests/utils.py
@@ -1,8 +1,55 @@
+import uuid
+from random import sample
+
 import numpy
+from numpy.testing import assert_equal
 
 from simphony.io.data_container_description import Data
 from simphony.core.data_container import DataContainer
 from simphony.core.cuba import CUBA
+from simphony.cuds.particles import Particle, Bond
+
+
+def compare_bonds(bond, reference, msg=None, testcase=None):
+    self = testcase
+    self.assertEqual(bond.uid, reference.uid)
+    self.assertEqual(bond.particles, reference.particles)
+    compare_data_containers(bond.data, bond.data, testcase=self)
+
+
+def compare_particles(particle, reference, msg=None, testcase=None):
+    self = testcase
+    self.assertEqual(particle.uid, reference.uid)
+    self.assertEqual(particle.coordinates, reference.coordinates)
+    compare_data_containers(particle.data, reference.data, testcase=self)
+
+
+def compare_data_containers(data, reference, msg=None, testcase=None):
+    self = testcase
+    self.assertEqual(len(data), len(reference))
+    for key in data:
+        self.assertIn(key, reference)
+        assert_equal(data[key], reference[key])
+
+
+def create_particles(n=10, restrict=None):
+    particle_list = []
+    for i in xrange(n):
+        data = create_data_container(restrict=restrict)
+        data[CUBA.VELOCITY] = i
+        particle_list.append(
+            Particle([i, i*10, i*100], data=data))
+    return particle_list
+
+
+def create_bonds(n=5, restrict=None):
+    bond_list = []
+    for i in xrange(n):
+        data = create_data_container(restrict=restrict)
+        data[CUBA.VELOCITY] = i
+        ids = [uuid.uuid4() for x in xrange(n)]
+        bond_list.append(Bond(particles=ids, data=data))
+    return bond_list
 
 
 def create_data_container(restrict=None):
