67c137b5be309946c35ebcba60ead2bb70465882
Ioannis Tziakos <ioannist@enthought.com>
2015-04-16 18:08:06 +0100

    add utility methods for mesh items

--- simphony/testing/utils.py
+++ simphony/testing/utils.py
@@ -7,6 +7,7 @@ from simphony.core.keywords import KEYWORDS
 from simphony.core.data_container import DataContainer
 from simphony.core.cuba import CUBA
 from simphony.cuds.particles import Particle, Bond
+from simphony.cuds.mesh import Point
 
 
 def compare_bonds(bond, reference, msg=None, testcase=None):
@@ -21,6 +22,20 @@ def compare_particles(particle, reference, msg=None, testcase=None):
     self.assertEqual(particle.uid, reference.uid)
     self.assertEqual(particle.coordinates, reference.coordinates)
     compare_data_containers(particle.data, reference.data, testcase=self)
+
+
+def compare_points(point, reference, msg=None, testcase=None):
+    self = testcase
+    self.assertEqual(point.uid, reference.uid)
+    self.assertEqual(point.coordinates, point.coordinates)
+    compare_data_containers(point.data, point.data, testcase=self)
+
+
+def compare_elements(element, reference, msg=None, testcase=None):
+    self = testcase
+    self.assertEqual(element.uid, reference.uid)
+    self.assertEqual(element.points, reference.points)
+    compare_data_containers(element.data, reference.data, testcase=self)
 
 
 def compare_lattice_nodes(node, reference, msg=None, testcase=None):
@@ -46,6 +61,16 @@ def create_particles(n=10, restrict=None):
         particle_list.append(
             Particle([i, i*10, i*100], data=data))
     return particle_list
+
+
+def create_points():
+    return [
+        Point((0.0, 0.0, 0.0)),
+        Point((1.0, 0.0, 0.0)),
+        Point((0.0, 1.0, 0.0)),
+        Point((0.0, 0.0, 1.0)),
+        Point((1.0, 0.0, 1.0)),
+        Point((0.0, 1.0, 1.0))]
 
 
 def create_bonds(n=5, restrict=None):
