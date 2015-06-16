93407e4f17a44de3c6cd872ab0e889aec788b41d
Ioannis Tziakos <ioannist@enthought.com>
2015-06-16 11:24:34 +0100

    cleanup and split lattice checks into class templates based on function

--- simphony/testing/abc_check_lattice.py
+++ simphony/testing/abc_check_lattice.py
@@ -8,11 +8,11 @@ from simphony.testing.utils import (
     create_data_container, compare_data_containers, compare_lattice_nodes)
 from simphony.cuds.lattice import (
     LatticeNode, make_square_lattice, make_rectangular_lattice,
-    make_orthorombicp_lattice)
+    make_orthorombicp_lattice, make_hexagonal_lattice, make_cubic_lattice)
 from simphony.core.data_container import DataContainer
 
 
-class ABCCheckLattice(object):
+class CheckLatticeNodeOperations(object):
 
     __metaclass__ = abc.ABCMeta
 
@@ -22,7 +22,7 @@ class ABCCheckLattice(object):
         self.addTypeEqualityFunc(
             LatticeNode, partial(compare_lattice_nodes, testcase=self))
         self.size = (5, 10, 15)
-        self.base_vect = (0.1, 0.2, 0.3)
+        self.base_vect = (0.2, 0.2, 0.2)
         self.origin = (-2.0, 0.0, 1.0)
         self.container = self.container_factory(
             'foo', 'Cubic', self.base_vect, self.size, self.origin)
@@ -141,16 +141,39 @@ class ABCCheckLattice(object):
         # Check that `new_node` is not the same instance as `node`
         self.assertIsNot(new_node, node)
 
+
+class CheckLatticeNodeCoordinates(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def container_factory(self, name, type_, base_vect, size, origin):
+        """ Create and return a lattice.
+        """
+
+    @abc.abstractmethod
+    def supported_cuba(self):
+        """ Return a list of CUBA keys to use for restricted containers.
+
+        """
+
     def test_get_coordinate_cubic(self):
-        container = self.container
-        xspace, yspace, zspace = self.base_vect
-        x, y, z = numpy.meshgrid(
-            range(self.size[0]), range(self.size[1]), range(self.size[2]))
+        default = make_cubic_lattice(
+            'Lattice3', 0.2, (5, 10, 15), (-2.0, 0.0, 1.0))
+        container = self.container_factory(
+            default.name,
+            default.type,
+            default.base_vect,
+            default.size,
+            default.origin)
+        xspace, yspace, zspace = default.base_vect
+        x, y, z = numpy.meshgrid(range(
+            default.size[0]), range(default.size[1]), range(default.size[2]))
         indexes = zip(x.flat, y.flat, z.flat)
         expected = zip(
-            x.ravel() * xspace + self.origin[0],
-            y.ravel() * yspace + self.origin[1],
-            z.ravel() * zspace + self.origin[2])
+            x.ravel() * xspace + default.origin[0],
+            y.ravel() * yspace + default.origin[1],
+            z.ravel() * zspace + default.origin[2])
 
         for i, index in enumerate(indexes):
             assert_array_equal(container.get_coordinate(index), expected[i])
@@ -208,6 +231,7 @@ class ABCCheckLattice(object):
             default.size,
             default.origin)
         xspace, yspace, zspace = default.base_vect
+
         x, y, z = numpy.meshgrid(
             range(default.size[0]),
             range(default.size[1]),
@@ -221,12 +245,56 @@ class ABCCheckLattice(object):
         for i, index in enumerate(indexes):
             assert_array_equal(container.get_coordinate(index), expected[i])
 
+    def test_get_coordinate_hexagonal(self):
+        default = make_hexagonal_lattice(
+            'Lattice4', (0.5, 0.54, 0.58), (15, 25, 35), (7, 9, 8))
+        container = self.container_factory(
+            default.name,
+            default.type,
+            default.base_vect,
+            default.size,
+            default.origin)
+        xspace, yspace, zspace = default.base_vect
+
+        for node in container.iter_nodes():
+            position = (
+                node.index[0] * xspace + 0.5 * xspace * (node.index[1] % 2),
+                node.index[1] * yspace,
+                container.origin[2])
+            assert_array_equal(container.get_coordinates[node.index], position)
+
+
+class CheckLatticeProperties(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    def setUp(self):
+        self.addTypeEqualityFunc(
+            DataContainer, partial(compare_data_containers, testcase=self))
+        self.addTypeEqualityFunc(
+            LatticeNode, partial(compare_lattice_nodes, testcase=self))
+        self.size = (5, 10, 15)
+        self.base_vect = (0.2, 0.2, 0.2)
+        self.origin = (-2.0, 0.0, 1.0)
+        self.container = self.container_factory(
+            'my_name', 'Cubic', self.base_vect, self.size, self.origin)
+
+    @abc.abstractmethod
+    def container_factory(self, name, type_, base_vect, size, origin):
+        """ Create and return a lattice.
+        """
+
+    @abc.abstractmethod
+    def supported_cuba(self):
+        """ Return a list of CUBA keys to use for restricted containers.
+        """
+
     def test_lattice_properties(self):
         container = self.container
 
         # check values
         self.assertEqual(container.type, 'Cubic')
-        self.assertEqual(container.name, 'foo')
+        self.assertEqual(container.name, 'my_name')
         assert_array_equal(container.size, self.size)
         assert_array_equal(container.origin, self.origin)
         assert_array_equal(container.base_vect, self.base_vect)
@@ -244,6 +312,51 @@ class ABCCheckLattice(object):
         with self.assertRaises(AttributeError):
             container.base_vect = self.base_vect
 
-        # check read-write
-        container.name = 'boo'
-        self.assertEqual(container.name, 'boo')
+    def test_container_name(self):
+        # given/when
+        container = self.container
+
+        # then
+        self.assertEqual(container.name, 'my_name')
+
+    def test_container_name_update(self):
+        # given
+        container = self.container
+
+        # when
+        container.name = 'new'
+
+        # then
+        self.assertEqual(container.name, 'new')
+
+    def test_container_data(self):
+        # when
+        container = self.container
+
+        # then
+        self.assertEqual(container.data, DataContainer())
+
+    def test_container_data_update(self):
+        # given
+        container = self.container
+        data = create_data_container(restrict=self.supported_cuba())
+
+        # when
+        container.data = data
+
+        # then
+        self.assertEqual(container.data, data)
+        self.assertIsNot(container.data, data)
+
+    def test_container_data_update_with_unsupported_cuba(self):
+        # given
+        container = self.container
+        data = create_data_container()
+        expected_data = create_data_container(restrict=self.supported_cuba())
+
+        # when
+        container.data = data
+
+        # then
+        self.assertEqual(container.data, expected_data)
+        self.assertIsNot(container.data, expected_data)
