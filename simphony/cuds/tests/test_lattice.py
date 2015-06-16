383c846e8be1f813a0b008e0315e4a423e0923a3
Ioannis Tziakos <ioannist@enthought.com>
2015-06-16 11:25:25 +0100

    rework tests to use the new templates and separate tests for factories

--- simphony/cuds/tests/test_lattice.py
+++ simphony/cuds/tests/test_lattice.py
@@ -6,11 +6,12 @@ from numpy.testing import assert_array_equal
 from math import sqrt
 
 from simphony.core.cuba import CUBA
-from simphony.testing.abc_check_lattice import ABCCheckLattice
+from simphony.testing.abc_check_lattice import (
+    CheckLatticeProperties, CheckLatticeNodeOperations,
+    CheckLatticeNodeCoordinates)
 from simphony.cuds.lattice import (
     Lattice, LatticeNode, make_cubic_lattice, make_rectangular_lattice,
     make_square_lattice, make_orthorombicp_lattice, make_hexagonal_lattice)
-from simphony.core.data_container import DataContainer
 
 
 class LatticeNodeTestCase(unittest.TestCase):
@@ -20,8 +21,7 @@ class LatticeNodeTestCase(unittest.TestCase):
         """Creation of a lattice node."""
         node = LatticeNode((0, 0, 0))
 
-        self.assertIsInstance(node, LatticeNode,
-                              "Error: not a LatticeNode!")
+        self.assertIsInstance(node, LatticeNode)
         self.assertEqual(node.index, (0, 0, 0))
 
     def test_construct_lattice_node_copy(self):
@@ -32,14 +32,13 @@ class LatticeNodeTestCase(unittest.TestCase):
 
         node_new = LatticeNode((2, 3, 1), node_org.data)
 
-        self.assertIsInstance(node_new, LatticeNode,
-                              "Error: not a LatticeNode!")
+        self.assertIsInstance(node_new, LatticeNode)
         self.assertEqual(node_new.index, (2, 3, 1))
         self.assertEqual(node_new.data[CUBA.DENSITY], 1.5)
         self.assertEqual(node_new.data[CUBA.VELOCITY], (0.2, -0.1, 0.0))
 
 
-class TestLattice(ABCCheckLattice, unittest.TestCase):
+class TestLatticeNodeOperations(CheckLatticeNodeOperations, unittest.TestCase):
 
     def container_factory(self, name, type_, base_vect, size, origin):
         return Lattice(name, type_, base_vect, size, origin)
@@ -47,88 +46,83 @@ class TestLattice(ABCCheckLattice, unittest.TestCase):
     def supported_cuba(self):
         return set(CUBA)
 
-    def test_construct_lattice_make(self):
-        """ Test creation of lattices using the factory functions.
 
-        """
+class TestLatticeNodeCoordinates(
+        CheckLatticeNodeCoordinates, unittest.TestCase):
 
-        hexag_lat = make_hexagonal_lattice('Lattice1', 0.1, (11, 21))
-        square_lat = make_square_lattice('Lattice2', 0.2, (12, 22))
-        rectang_lat = make_rectangular_lattice(
-            'Lattice3', (0.3, 0.35), (13, 23))
-        cubic_lat = make_cubic_lattice(
-            'Lattice4', 0.4, (14, 24, 34), (4, 5, 6))
-        orthop_lat = make_orthorombicp_lattice(
+    def container_factory(self, name, type_, base_vect, size, origin):
+        return Lattice(name, type_, base_vect, size, origin)
+
+    def supported_cuba(self):
+        return set(CUBA)
+
+    @unittest.skip('Hexagonal coordinates are not supported yet')
+    def test_get_coordinate_hexagonal(self):
+        pass
+
+
+class TestLatticeProperties(CheckLatticeProperties, unittest.TestCase):
+
+    def container_factory(self, name, type_, base_vect, size, origin):
+        return Lattice(name, type_, base_vect, size, origin)
+
+    def supported_cuba(self):
+        return set(CUBA)
+
+
+class TestLatticeFactories(unittest.TestCase):
+
+    def test_make_hexagonal(self):
+        lattice = make_hexagonal_lattice('Lattice1', 0.1, (11, 21))
+
+        self.assertIsInstance(lattice, Lattice)
+        self.assertEqual(lattice.name, 'Lattice1')
+        self.assertEqual(lattice.type, 'Hexagonal')
+        assert_array_equal(lattice.size, (11, 21, 1))
+        assert_array_equal(lattice.origin, (0, 0, 0))
+        assert_array_equal(lattice.base_vect,
+                           (0.5*0.1, 0.5*sqrt(3)*0.1, 0))
+
+    def test_make_square(self):
+        lattice = make_square_lattice('Lattice2', 0.2, (12, 22))
+
+        self.assertIsInstance(lattice, Lattice)
+        self.assertEqual(lattice.name, 'Lattice2')
+        self.assertEqual(lattice.type, 'Square')
+        assert_array_equal(lattice.size, (12, 22, 1))
+        assert_array_equal(lattice.origin, (0, 0, 0))
+        assert_array_equal(lattice.base_vect, (0.2, 0.2, 0))
+
+    def test_make_cubic(self):
+        lattice = make_cubic_lattice('Lattice4', 0.4, (14, 24, 34), (4, 5, 6))
+
+        self.assertIsInstance(lattice, Lattice)
+        self.assertEqual(lattice.name, 'Lattice4')
+        self.assertEqual(lattice.type, 'Cubic')
+        assert_array_equal(lattice.size, (14, 24, 34))
+        assert_array_equal(lattice.origin, (4, 5, 6))
+        assert_array_equal(lattice.base_vect, (0.4, 0.4, 0.4))
+
+    def test_make_rectangular(self):
+        lattice = make_rectangular_lattice('Lattice3', (0.3, 0.35), (13, 23))
+
+        self.assertIsInstance(lattice, Lattice)
+        self.assertEqual(lattice.name, 'Lattice3')
+        self.assertEqual(lattice.type, 'Rectangular')
+        assert_array_equal(lattice.size, (13, 23, 1))
+        assert_array_equal(lattice.origin, (0, 0, 0))
+        assert_array_equal(lattice.base_vect, (0.3, 0.35, 0))
+
+    def test_orthorombicp_lattice(self):
+        lattice = make_orthorombicp_lattice(
             'Lattice5', (0.5, 0.54, 0.58), (15, 25, 35), (7, 8, 9))
 
-        self.assertIsInstance(hexag_lat, Lattice, "Error: not a Lattice!")
-        self.assertIsInstance(square_lat, Lattice, "Error: not a Lattice!")
-        self.assertIsInstance(rectang_lat, Lattice, "Error: not a Lattice!")
-        self.assertIsInstance(cubic_lat, Lattice, "Error: not a Lattice!")
-        self.assertIsInstance(orthop_lat, Lattice, "Error: not a Lattice!")
-
-        self.assertEqual(hexag_lat.name, 'Lattice1')
-        self.assertEqual(square_lat.name, 'Lattice2')
-        self.assertEqual(rectang_lat.name, 'Lattice3')
-        self.assertEqual(cubic_lat.name, 'Lattice4')
-        self.assertEqual(orthop_lat.name, 'Lattice5')
-
-        self.assertEqual(hexag_lat.type, 'Hexagonal')
-        self.assertEqual(square_lat.type, 'Square')
-        self.assertEqual(rectang_lat.type, 'Rectangular')
-        self.assertEqual(cubic_lat.type, 'Cubic')
-        self.assertEqual(orthop_lat.type, 'OrthorombicP')
-
-        assert_array_equal(hexag_lat.size, (11, 21, 1))
-        assert_array_equal(square_lat.size, (12, 22, 1))
-        assert_array_equal(rectang_lat.size, (13, 23, 1))
-        assert_array_equal(cubic_lat.size, (14, 24, 34))
-        assert_array_equal(orthop_lat.size, (15, 25, 35))
-
-        assert_array_equal(hexag_lat.origin, (0, 0, 0))
-        assert_array_equal(square_lat.origin, (0, 0, 0))
-        assert_array_equal(rectang_lat.origin, (0, 0, 0))
-        assert_array_equal(cubic_lat.origin, (4, 5, 6))
-        assert_array_equal(orthop_lat.origin, (7, 8, 9))
-
-        assert_array_equal(hexag_lat.base_vect,
-                           (0.5*0.1, 0.5*sqrt(3)*0.1, 0))
-        assert_array_equal(square_lat.base_vect, (0.2, 0.2, 0))
-        assert_array_equal(rectang_lat.base_vect, (0.3, 0.35, 0))
-        assert_array_equal(cubic_lat.base_vect, (0.4, 0.4, 0.4))
-        assert_array_equal(orthop_lat.base_vect, (0.5, 0.54, 0.58))
-
-    def test_set_modify_data(self):
-        """ Check that data can be retrieved and is consistent. Check that
-        the internal data of the lattice cannot be modified outside the
-        lattice class
-        """
-        lattice = self.container_factory('test_lat', 'Cubic',
-                                         (1.0, 1.0, 1.0), (4, 3, 2),
-                                         (0, 0, 0))
-        org_data = DataContainer()
-
-        org_data[CUBA.VELOCITY] = (0, 0, 0)
-
-        lattice.data = org_data
-        ret_data = lattice.data
-
-        self.assertEqual(org_data, ret_data)
-        self.assertIsNot(org_data, ret_data)
-
-        org_data = DataContainer()
-
-        org_data[CUBA.VELOCITY] = (0, 0, 0)
-
-        lattice.data = org_data
-        mod_data = lattice.data
-
-        mod_data[CUBA.VELOCITY] = (1, 1, 1)
-
-        ret_data = lattice.data
-
-        self.assertEqual(org_data, ret_data)
-        self.assertIsNot(org_data, ret_data)
+        self.assertIsInstance(lattice, Lattice)
+        self.assertEqual(lattice.name, 'Lattice5')
+        self.assertEqual(lattice.type, 'OrthorombicP')
+        assert_array_equal(lattice.size, (15, 25, 35))
+        assert_array_equal(lattice.origin, (7, 8, 9))
+        assert_array_equal(lattice.base_vect, (0.5, 0.54, 0.58))
 
 
 if __name__ == '__main__':
