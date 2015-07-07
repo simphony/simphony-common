f5ec96e5b1ffe30edc8c3eac5b6946be435d7f04
tuopuu <tuomas.a.puurtinen@jyu.fi>
2015-07-07 10:57:19 +0300

    Changing update_node to update_nodes. The code itself, tests, docstrings and uml diagrams should be all done in this commit.

--- simphony/io/h5_cuds.py
+++ simphony/io/h5_cuds.py
@@ -155,8 +155,7 @@ class H5CUDS(object):
             group, lattice.type, lattice.base_vect,
             lattice.size, lattice.origin)
         h5_lattice.data = lattice.data
-        for node in lattice.iter_nodes():
-            h5_lattice.update_node(node)
+        h5_lattice.update_nodes(lattice.iter_nodes())
 
         return h5_lattice
 
