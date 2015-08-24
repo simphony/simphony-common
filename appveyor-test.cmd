a0e07f821710c1aae76fb401179916cf81ed851d
Ioannis Tziakos <ioannist@enthought.com>
2015-08-24 13:53:23 +0100

    fix: name of build command

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -5,6 +5,6 @@ flake8 .
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage run -m unittest discover -p test*
 if %errorlevel% neq 0 exit /b %errorlevel%
-python setup.py build_shpinx
+python setup.py build_sphinx
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report
