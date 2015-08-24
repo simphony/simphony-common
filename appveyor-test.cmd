ad9eb73bbd4ad084e8be678de01222ac67fbbf81
Ioannis Tziakos <ioannist@enthought.com>
2015-08-24 13:53:48 +0100

    provide verbose output for the test run

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -3,7 +3,7 @@ call setenv /x64
 
 flake8 .
 if %errorlevel% neq 0 exit /b %errorlevel%
-coverage run -m unittest discover -p test*
+coverage run -m unittest discover -v -p test*
 if %errorlevel% neq 0 exit /b %errorlevel%
 python setup.py build_sphinx
 if %errorlevel% neq 0 exit /b %errorlevel%
