ae6814dbbd6f3e71c19010d7dd162dff039aede4
Carlos Roig <roigcarlo@gmail.com>
2015-12-11 17:04:43 +0100

    Material relation now stores the possible number of materials

--- simphony/material_relations/material_relation.py
+++ simphony/material_relations/material_relation.py
@@ -23,17 +23,38 @@ class MaterialRelation(ABCMaterialRelation):
     materials: list of uids
         materials where this relation applies
 
+    num_materials: list of int
+        list with all possible configurations of avaliable
+        number of materials in the relation
+
     kind: CUBA
         Describes the kind of the MaterialRelation
+
+    Raises
+    ------
+    ValueError :
+        If the number of materials does not match with any value of
+        num_materials
 
     """
 
     def __init__(self, name, description, parameters, supported_parameters,
-                 materials, kind):
+                 materials, num_materials, kind):
+
+        self._num_materials = num_materials
+
+        if(materials.size() not in self._num_materials):
+            error_str = "Incorrect number of materials, expected: {}"
+            raise ValueError(
+                error_str.format(
+                    materials.size(),
+                    self._num_materials
+                )
+            )
 
         self.name = name
-        self.description = {}
-        self.parameters = {}
-        self.supported_parameters = {}
-        self.materials = {}
-        self.kind = {}
+        self.description = description
+        self.parameters = parameters
+        self.supported_parameters = supported_parameters
+        self.materials = materials
+        self.kind = kind
