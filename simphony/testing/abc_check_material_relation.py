02f58041d0360fe2577e701f5c2746a559fdfef2
Carlos Roig <roigcarlo@gmail.com>
2015-12-14 13:04:03 +0100

    Updated tests

--- simphony/testing/abc_check_material_relation.py
+++ simphony/testing/abc_check_material_relation.py
@@ -7,11 +7,10 @@ class CheckMaterialRelation(object):
     __metaclass__ = abc.ABCMeta
 
     def setUp(self):
-        self._kind = retrieve_kind()
         pass
 
     @abc.abstractmethod
-    def retrieve_kind():
+    def get_kind():
         """ Returns the kinf of the tested relation
 
         """
@@ -30,6 +29,8 @@ class CheckMaterialRelation(object):
         """
 
         relation = self.container_factory('foo_relation')
+        original_name = relation.name
+        original_name = 'foo_relation_2'
 
         assertEqual(relation.name, 'foo_relation')
 
@@ -39,9 +40,9 @@ class CheckMaterialRelation(object):
         """
 
         relation = self.container_factory('foo_relation')
-        relation.name = 'other_name'
+        relation.name = 'foo_relation_2'
 
-        assertEqual(relation.name, 'other_name')
+        assertEqual(relation.name, 'foo_relation_2')
 
     def test_material_relation_invalid_name_update(self):
         """ Test that name is updated correctly
@@ -57,13 +58,31 @@ class CheckMaterialRelation(object):
         """ Test that material relation parameteres are set correctly
 
         """
-        pass
+
+        # when
+        relation = self.container_factory('foo_relation')
+
+        # then
+        self.assertEqual(relation._parameters, DataContainer())
 
     def test_material_relation_parameters_update(self):
         """ Test that material relation parameteres are updated correctly
 
         """
-        pass
+
+        # given
+        relation = self.container_factory('foo_relation')
+        data = create_data_container()
+        expected_data = create_data_container(
+            restrict=self.supported_parameters
+        )
+
+        # when
+        relation.data = data
+
+        # then
+        self.assertEqual(relation.data, expected_data)
+        self.assertIsNot(relation.data, expected_data)
 
     def test_material_relation_supported_parameters(self):
         """ Test that name is set correctly
@@ -85,6 +104,12 @@ class CheckMaterialRelation(object):
         """ Test that name is set correctly
 
         """
+
+        relation = self.container_factory('foo_relation')
+        relation.name = 'foo_relation_2'
+
+        assertEqual(relation.name, 'foo_relation_2')
+
         pass
 
     def test_material_relation_materials_update(self):
@@ -100,7 +125,7 @@ class CheckMaterialRelation(object):
 
         relation = self.container_factory('foo_relation')
 
-        assertEqual(self.kind, self.retrieve_kind())
+        assertEqual(self.kind, self.get_kind())
 
         pass
 
