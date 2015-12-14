2dcb1f24b190dc73a81c4fee49b2021864077020
Carlos Roig <roigcarlo@gmail.com>
2015-12-14 11:39:59 +0100

    Adding some initial tests

---
+++ simphony/testing/abc_check_material_relation.py
@@ -0,0 +1,127 @@
+import abc
+import uuid
+
+
+class CheckMaterialRelation(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    def setUp(self):
+        self._kind = retrieve_kind()
+        pass
+
+    @abc.abstractmethod
+    def retrieve_kind():
+        """ Returns the kinf of the tested relation
+
+        """
+        pass
+
+    @abc.abstractmethod
+    def container_factory(self, number):
+        """ Create and return a given number of material relations.
+
+        """
+        pass
+
+    def test_material_relation_name(self):
+        """ Test that name is set correctly
+
+        """
+
+        relation = self.container_factory('foo_relation')
+
+        assertEqual(relation.name, 'foo_relation')
+
+    def test_material_relation_name_update(self):
+        """ Test that name is updated correctly
+
+        """
+
+        relation = self.container_factory('foo_relation')
+        relation.name = 'other_name'
+
+        assertEqual(relation.name, 'other_name')
+
+    def test_material_relation_invalid_name_update(self):
+        """ Test that name is updated correctly
+
+        """
+
+        relation = self.container_factory('foo_relation')
+
+        with self.assertRaises(TypeError):
+            relation.name = 42
+
+    def test_material_relation_data(self):
+        """ Test that data is set correctly
+
+        """
+        pass
+
+    def test_material_relation_data_update(self):
+        """ Test that data is updated correctly
+
+        """
+        pass
+
+    def test_material_relation_parameters(self):
+        """ Test that material relation parameteres are set correctly
+
+        """
+        pass
+
+    def test_material_relation_parameters_update(self):
+        """ Test that material relation parameteres are updated correctly
+
+        """
+        pass
+
+    def test_material_relation_supported_parameters(self):
+        """ Test that name is set correctly
+
+        """
+        pass
+
+    def test_material_relation_supported_parameters_update(self):
+        """ Test that name is updated correctly
+
+        """
+
+        relation = self.container_factory('foo_relation')
+
+        with self.assertRaises(AttributeError):
+            relation.kind = "invalid attribute"
+
+    def test_material_relation_materials(self):
+        """ Test that name is set correctly
+
+        """
+        pass
+
+    def test_material_relation_materials_update(self):
+        """ Test that name is updated correctly
+
+        """
+        pass
+
+    def test_material_relation_kind(self):
+        """ Test that kind is set correctly
+
+        """
+
+        relation = self.container_factory('foo_relation')
+
+        assertEqual(self.kind, self.retrieve_kind())
+
+        pass
+
+    def test_material_relation_kind_update(self):
+        """ Test that kind can't be accessed
+
+        """
+
+        relation = self.container_factory('foo_relation')
+
+        with self.assertRaises(AttributeError):
+            relation.kind = "invalid attribute"
