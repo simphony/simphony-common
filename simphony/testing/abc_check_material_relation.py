d743c6ff3c5b7649392ab2540f78df01918c6357
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-12-16 13:43:23 +0100

    Change tests

--- simphony/testing/abc_check_material_relation.py
+++ simphony/testing/abc_check_material_relation.py
@@ -1,5 +1,4 @@
 import abc
-from simphony.core.data_container import DataContainer
 
 from simphony.testing.utils import create_data_container
 
@@ -12,58 +11,21 @@ class CheckMaterialRelation(object):
         pass
 
     @abc.abstractmethod
-    def get_name():
-        """ Returns the name of the tested relation
+    def container_factory(self, name):
+        """ Create and return a material relations with the default values
 
         """
-        pass
 
-    @abc.abstractmethod
-    def get_kind():
-        """ Returns the kind of the tested relation
-
-        """
-        pass
-
-    @abc.abstractmethod
-    def container_factory(self, name=None, materials=None):
-        """ Create and return a given number of material relations.
-
-        """
-        pass
-
-    def test_material_relation_name(self):
-        """ Test that name is set correctly
+    def test_material_relation_name_update(self):
+        """ Test that name is set correctly and updated correctly
 
         """
 
         relation = self.container_factory('foo_relation')
-        original_name = relation.name
-        original_name = 'foo_relation_2'
-
         self.assertEqual(relation.name, 'foo_relation')
-        self.assertNotEqual(relation.name, original_name)
 
-    def test_material_relation_name_default(self):
-        """ Test that name is set correctly
 
-        """
-
-        relation = self.container_factory()
-        original_name = relation.name
-        original_name = 'foo_relation_2'
-
-        self.assertEqual(relation.name, self.get_name())
-        self.assertNotEqual(relation.name, original_name)
-
-    def test_material_relation_name_update(self):
-        """ Test that name is updated correctly
-
-        """
-
-        relation = self.container_factory()
         relation.name = 'foo_relation_2'
-
         self.assertEqual(relation.name, 'foo_relation_2')
 
     def test_material_relation_invalid_name_update(self):
@@ -75,15 +37,6 @@ class CheckMaterialRelation(object):
 
         with self.assertRaises(TypeError):
             relation.name = 42
-
-    def test_material_relation_description(self):
-        """ Test that description is set correctly
-
-        """
-
-        relation = self.container_factory('foo_relation')
-
-        self.assertNotEqual(relation.description, None)
 
     def test_material_relation_description_update(self):
         """ Test that description is updated correctly
@@ -115,7 +68,8 @@ class CheckMaterialRelation(object):
         relation = self.container_factory('foo_relation')
 
         # then
-        self.assertEqual(relation._parameters, DataContainer())
+        # TODO should be testing that it is created with defaults
+        # self.assertEqual(relation._parameters, DataContainer())
 
     def test_material_relation_parameters_update(self):
         """ Test that material relation parameters are updated correctly
@@ -126,8 +80,7 @@ class CheckMaterialRelation(object):
         relation = self.container_factory('foo_relation')
 
         parameters = create_data_container(
-            restrict=relation.supported_parameters
-        )
+            restrict=relation.supported_parameters)
 
         # when
         relation.parameters = parameters
@@ -137,43 +90,19 @@ class CheckMaterialRelation(object):
         self.assertIsNot(relation.parameters, parameters)
 
     def test_material_relation_supported_parameters(self):
-        """ Test that name is set correctly
-
-        """
         pass
 
     def test_material_relation_supported_parameters_update(self):
-        """ Test that name is updated correctly
-
-        """
-
         relation = self.container_factory('foo_relation')
 
         with self.assertRaises(AttributeError):
             relation.kind = "invalid attribute"
 
-    def test_material_relation_materials(self):
-        """ Test that name is set correctly
-
-        """
-
     def test_material_relation_materials_update(self):
-        """ Test that name is updated correctly
-
-        """
         pass
 
-    def test_material_relation_kind(self):
-        """ Test that kind is set correctly
-
-        """
-        pass
 
     def test_material_relation_kind_update(self):
-        """ Test that kind can't be accessed
-
-        """
-
         relation = self.container_factory('foo_relation')
 
         with self.assertRaises(AttributeError):
