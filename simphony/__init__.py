864b8da9b2823934aaa986d9bc8cc3ff92cddbf9
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2016-01-28 16:00:12 +0100

    fix flake8 warning

--- simphony/__init__.py
+++ simphony/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-"""SimPhoNy: Simulation framework for multi-scale phenomena in micro- and nanosystems.
+"""SimPhoNy: Simulation framework for multi-scale phenomena in micro-
+ and nanosystems.
 
-SimPhoNy is an EU-project funded by the 7th Framework Programme (Project number 604005)
-under the call NMP.2013.1.4-1: "Development of an integrated multi-scale modelling
-environment for nanomaterials and systems by design". SimPhoNy is a collaborative
-research project running from January 1st 2014 until December 31st 2016.
+SimPhoNy is an EU-project funded by the 7th Framework Programme (Project
+ number 604005) under the call NMP.2013.1.4-1: "Development of an integrated
+ multi-scale modelling environment for nanomaterials and systems by design".
+ SimPhoNy is a collaborative research project running from January 1st 2014
+ until December 31st 2016.
 
 For more information see: http://www.simphony-project.eu/.
 
