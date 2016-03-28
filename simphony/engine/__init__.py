fb3c9ca947b1cfd99be924cbd02cc61f6c42d594
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2016-03-28 17:37:18 +0200

    Add wrapper query functions to engine package. close #285.

--- simphony/engine/__init__.py
+++ simphony/engine/__init__.py
@@ -7,6 +7,25 @@ registered at the 'simphony.engine' entry point.
 
 """
 
+# A temporary solution to keep a list of wrappers known to SimPhoNy
+_WRAPPER_REGISTRY = {}
+
+
+def get_wrapper(wrapper_name):
+    """Return corresponding wrapper class.
+
+    Parameters
+    ----------
+    wrapper_name: str
+        name that wrapper is registered with.
+    """
+    return _WRAPPER_REGISTRY.get(wrapper_name)
+
+
+def get_wrappers():
+    """Return a list of know wrappers."""
+    return _WRAPPER_REGISTRY.keys()
+
 
 def load_engine_extentions():
     """ Discover and load engine extension modules.
@@ -16,7 +35,15 @@ def load_engine_extentions():
     mgr = extension.ExtensionManager(
         namespace='simphony.engine',
         invoke_on_load=False)
-    return {extension.name: extension.plugin for extension in mgr.extensions}
+    extensions = {}
+    for extension in mgr.extensions:
+        extensions[extension.name] = extension.plugin
+        # Invoke 'get_wrappers' method and register wrappers.
+        if hasattr(extension.plugin, 'get_wrappers'):
+            wrappers = extension.plugin.get_wrappers()
+            if wrappers:
+                _WRAPPER_REGISTRY.update(wrappers)
+    return extensions
 
 
 # Populate the module namespace
