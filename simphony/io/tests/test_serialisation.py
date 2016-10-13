899b67c81db5de5e48bb6f28191006ccb5b023c5
tuopuu <tuomas.a.puurtinen@jyu.fi>
2016-10-13 15:11:44 +0300

    Pep8 fix.

--- simphony/io/tests/test_serialisation.py
+++ simphony/io/tests/test_serialisation.py
@@ -37,7 +37,7 @@ class TestSerialization(unittest.TestCase):
     def test_save_CUDS_name(self):
         name = 'somename'
         filename = os.path.join(self.temp_dir, 'test_named.yml')
-        C = CUDS(name = name)
+        C = CUDS(name=name)
         with closing(open(filename,'w')) as handle:
             save_CUDS(handle, C)
 
@@ -50,7 +50,7 @@ class TestSerialization(unittest.TestCase):
     def test_save_CUDS_description(self):
         description = 'some very long description'
         filename = os.path.join(self.temp_dir, 'test_description.yml')
-        C = CUDS(description = description)
+        C = CUDS(description=description)
         with closing(open(filename,'w')) as handle:
             save_CUDS(handle, C)
 
@@ -61,7 +61,7 @@ class TestSerialization(unittest.TestCase):
 
     def test_save_CUDS_empty(self):
         filename = os.path.join(self.temp_dir, 'test_empty.yml')
-        C = CUDS(name = 'empty', description = 'just an empty model')
+        C = CUDS(name='empty', description='just an empty model')
         with closing(open(filename,'w')) as handle:
             save_CUDS(handle, C)
 
@@ -78,7 +78,7 @@ class TestSerialization(unittest.TestCase):
 
     def test_save_CUDS_full(self):
         filename = os.path.join(self.temp_dir, 'test_full.yml')
-        C = CUDS(name = 'full', description = 'fully randomized model')
+        C = CUDS(name='full', description='fully randomized model')
 
         for key in KEYWORDS.keys():
             if key not in ['VERSION']:
@@ -86,11 +86,11 @@ class TestSerialization(unittest.TestCase):
                 if comp:
                     C.add(comp)
 
-        with closing(open(filename,'w')) as handle:
+        with closing(open(filename, 'w')) as handle:
             save_CUDS(handle, C)
 
         CC = None
-        with closing(open(filename,'r')) as handle:
+        with closing(open(filename, 'r')) as handle:
             CC = load_CUDS(handle)
 
         self.assertEqual(CC.name, C.name)
@@ -104,12 +104,12 @@ class TestSerialization(unittest.TestCase):
         if dtype == numpy.int32:
             if shape == []:
                 shape = [1]
-            return numpy.random.randint(-9999999,9999999,size = shape)
+            return numpy.random.randint(-9999999, 9999999, size=shape)
         elif dtype == numpy.float64:
             if shape == [] or shape == None:
                 return numpy.random.rand(3)
             elif len(shape) == 2:
-                return numpy.random.rand(shape[0],shape[1])
+                return numpy.random.rand(shape[0], shape[1])
             elif len(shape) == 1:
                 if shape[0] == 1:
                     return numpy.random.rand(1)[0]
@@ -117,7 +117,7 @@ class TestSerialization(unittest.TestCase):
                     return numpy.random.rand(shape[0])
         elif dtype == str:
             return ''.join(random.choice(string.ascii_uppercase +
-                string.digits) for _ in range(shape[0]))
+                    string.digits) for _ in range(shape[0]))
         elif dtype == bool:
             return random.choice([True, False])
 
@@ -132,12 +132,12 @@ class TestSerialization(unittest.TestCase):
                 data_dict = {}
                 for prm in comp_inst.supported_parameters():
                     if prm is not CUBA.UUID:
-                        name = str(prm).replace('CUBA.','')
+                        name = str(prm).replace('CUBA.', '')
                         if to_camel_case(name) in dir(api):
                             # Create one or two subcomponents if supported
                             if numpy.random.randint(2):
-                                subcomp1 =
-                                    self._create_random_CUDSComponent(name)
+                                subcomp1
+                                    = self._create_random_CUDSComponent(name)
                                 data_dict[prm] = subcomp1
                             else:
                                 subcomp1 = random_component(name)
