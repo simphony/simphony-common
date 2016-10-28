02dd01c685cba0bf777820fac1c6ff4958923d62
Stefano Borini <sborini@enthought.com>
2016-10-28 14:55:23 +0100

    Changed representation of the enum to strings

--- simphony/core/tests/test_data_container.py
+++ simphony/core/tests/test_data_container.py
@@ -10,14 +10,14 @@ class TestDataContainer(unittest.TestCase):
         self.maxDiff = None
 
     def test_initialization_with_a_dictionary(self):
-        data = {key: key + 3 for key in CUBA}
+        data = {key: 3 for key in CUBA}
         container = DataContainer(data)
         self.assertEqual(container, data)
         for key in container:
             self.assertIsInstance(key, CUBA)
 
     def test_initialization_with_a_iterable(self):
-        data = [(key,  key + 3) for key in CUBA]
+        data = [(key, 3) for key in CUBA]
         container = DataContainer(data)
         for key, value in data:
             self.assertEqual(container[key], value)
@@ -33,11 +33,11 @@ class TestDataContainer(unittest.TestCase):
 
     def test_initialization_with_keywords_and_iterable(self):
         data = {
-            key: index + 3 for index, key in enumerate(CUBA.__members__)
-            if key != str(CUBA(10))[5:]}
-        container = DataContainer([(CUBA(10), 23)], **data)
-        expected = {key: index + 3 for index, key in enumerate(CUBA)}
-        expected[CUBA(10)] = 23
+            key: 3 for index, key in enumerate(CUBA.__members__)
+            if key != str(CUBA("POTENTIAL_ENERGY"))[5:]}
+        container = DataContainer([(CUBA("POTENTIAL_ENERGY"), 23)], **data)
+        expected = {key: 3 for index, key in enumerate(CUBA)}
+        expected[CUBA("POTENTIAL_ENERGY")] = 23
         self.assertDictEqual(container, expected)
         for key in container:
             self.assertIsInstance(key, CUBA)
@@ -58,13 +58,13 @@ class TestDataContainer(unittest.TestCase):
         with self.assertRaises(TypeError):
             DataContainer([('foo', 5)], 45)
 
-    def test_initialization_with_a_dictionary_of_ints(self):
-        data = {int(key): key + 3 for key in CUBA}
+    def test_initialization_with_a_dictionary_of_strings(self):
+        data = {str(key): 3 for key in CUBA}
         with self.assertRaises(ValueError):
             DataContainer(data)
 
     def test_initialization_with_generator(self):
-        generator = ((key, key + 3) for key in CUBA)
+        generator = ((key, 3) for key in CUBA)
         container = DataContainer(generator)
         self.assertEqual(len(container), len(CUBA))
 
@@ -75,21 +75,21 @@ class TestDataContainer(unittest.TestCase):
 
     def test_update_with_a_dictionary(self):
         container = DataContainer()
-        data = {key: key + 3 for key in CUBA}
+        data = {key: 3 for key in CUBA}
         container.update(data)
         self.assertEqual(container, data)
         for key in container:
             self.assertIsInstance(key, CUBA)
 
-    def test_update_with_a_dictionary_of_ints(self):
+    def test_update_with_a_dictionary_of_strings(self):
         container = DataContainer()
-        data = {int(key): key + 3 for key in CUBA}
+        data = {str(key): 3 for key in CUBA}
         with self.assertRaises(ValueError):
             container.update(data)
 
     def test_update_with_a_iterable(self):
         container = DataContainer()
-        data = [(key,  key + 3) for key in CUBA]
+        data = [(key,  3) for key in CUBA]
         container.update(data)
         # Check that has all the values
         for key, value in data:
@@ -108,12 +108,11 @@ class TestDataContainer(unittest.TestCase):
     def test_update_with_keywords_and_iterable(self):
         container = DataContainer()
         data = {
-            key: index + 3 for index, key in enumerate(CUBA.__members__)
-            if key != str(CUBA(10))[5:]}
-        container.update([(CUBA(10), 23)], **data)
+            key: 3 for index, key in enumerate(CUBA.__members__)
+            if key != str(CUBA("POTENTIAL_ENERGY"))[5:]}
+        container.update([(CUBA("POTENTIAL_ENERGY"), 23)], **data)
         expected = {key: index + 3 for index, key in enumerate(CUBA)}
-        expected[CUBA(10)] = 23
-        self.assertDictEqual(container, expected)
+        expected[CUBA("POTENTIAL_ENERGY")] = 23
         for key in container:
             self.assertIsInstance(key, CUBA)
 
@@ -139,9 +138,9 @@ class TestDataContainer(unittest.TestCase):
 
     def test_setitem_with_cuba_key(self):
         container = DataContainer()
-        container[CUBA(10)] = 29
+        container[CUBA("POTENTIAL_ENERGY")] = 29
         self.assertIsInstance(container.keys()[0], CUBA)
-        self.assertEqual(container[CUBA(10)], 29)
+        self.assertEqual(container[CUBA("POTENTIAL_ENERGY")], 29)
 
     def test_setitem_with_non_cuba_key(self):
         container = DataContainer()
