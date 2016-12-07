d0a7b45e7ca2c366644e2d0899f9de49774f0ce1
Stefano Borini <sborini@enthought.com>
2016-12-07 14:40:55 +0000

    Removed appveyor need to duplicate information
    (cherry picked from commit 34cd971f1c274c2d76335bd5e8faf18f3a974e8f)

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -2,20 +2,7 @@
 call setenv /x64
 
 rem install python packages
-pip install numpy
-rem builds currently fail
-rem pip install numexpr
-rem pip install tables >= 3.1.1
-pip install enum34>=1.0.4
-pip install stevedore>=1.2.0
-pip install click>=3.3
-pip install pyyaml>=3.11
-pip install sphinx>=1.3.1
-pip install tabulate>=0.7.4
-pip install mock==1.0.1
-pip install coverage
-pip install flake8==2.6.2
-pip install hypothesis
+pip install -r dev_requirements.txt
 
 rem install simphony-common 
 python setup.py develop
