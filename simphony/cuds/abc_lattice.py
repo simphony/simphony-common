2e5c551c223dbe46f768fe316537c80fe15c22cc
Stefano Borini <sborini@enthought.com>
2017-01-11 16:26:34 +0000

    removed deprecated methods from lattice and particles

--- simphony/cuds/abc_lattice.py
+++ simphony/cuds/abc_lattice.py
@@ -2,7 +2,6 @@ from abc import abstractmethod
 
 from ..core import CUBA
 from .abc_dataset import ABCDataset
-from .utils import deprecated
 
 
 class ABCLattice(ABCDataset):
@@ -136,62 +135,6 @@ class ABCLattice(ABCDataset):
             The number of items of item_type in the dataset.
         """
         return self.count_of(CUBA.NODE)
-
-    @deprecated
-    def get_node(self, index):  # pragma: no cover
-        """
-        Deprecated. Use get() instead.
-
-        Get the lattice node corresponding to the given index.
-
-        Parameters
-        ----------
-        index : int[3]
-            node index coordinate
-
-        Returns
-        -------
-        node : LatticeNode
-
-        """
-        return self.get(index)
-
-    @deprecated
-    def update_nodes(self, nodes):  # pragma: no cover
-        """
-        Deprecated. Use update() instead.
-
-        Update the corresponding lattice nodes.
-
-        Parameters
-        ----------
-        nodes : iterator of LatticeNodes
-
-        """
-        self.update(nodes)
-
-    @deprecated
-    def iter_nodes(self, indices=None):  # pragma: no cover
-        """
-        Deprecated. Use iter() instead.
-
-        Get an iterator over the LatticeNodes described by the indices.
-
-        Parameters
-        ----------
-        indices : iterable set of int[3], optional
-            When indices (i.e. node index coordinates) are provided, then nodes
-            are returned in the same order of the provided indices. If indices
-            is None, there is no restriction on the order of the returned
-            nodes.
-
-        Returns
-        -------
-        iterator:
-            An iterator over LatticeNode objects
-
-        """
-        return self.iter(indices)
 
     @property
     def primitive_cell(self):
