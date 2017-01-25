9a9ed87aa4b749ba93a323e8e2d7c64d664fd500
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2017-01-25 15:18:13 +0100

    Add CUBA-CUDS mapper utility.

--- simphony/cuds/utils.py
+++ simphony/cuds/utils.py
@@ -1,5 +1,8 @@
-from functools import wraps
 import warnings
+import importlib
+from functools import wraps
+
+_CUBA_CUDS_MAP = None
 
 
 def deprecated(func):
@@ -9,3 +12,40 @@ def deprecated(func):
         return func(*args, **kwargs)
 
     return _deprecated
+
+
+def turn_cuba_into_cuds(cuba_key):
+	"""Return the equivalent CUDS class for the given CUBA key.
+
+	Parameters
+	----------
+	cuba_key: CUBA
+		The key to find its equivalent CUDS class.
+
+	Raises
+	------
+	ValueError:
+		If no CUDS exists for the given CUBA key.
+
+	Returns
+	-------
+	object: type
+	"""
+	global _CUBA_CUDS_MAP
+	if not _CUBA_CUDS_MAP:
+		_fill_cuba_cuds_map()
+
+	if cuba_key not in _CUBA_CUDS_MAP:
+		raise ValueError('No CUDS class exist for {cuba_key}'
+			.format(cuba_key=cuba_key))
+
+	return _CUBA_CUDS_MAP[cuba_key]
+
+
+def _fill_cuba_cuds_map():
+	"""Fill the cuba-cuds map."""
+	api_mod = importlib.import_module('simphony.cuds.meta.api')
+	global _CUBA_CUDS_MAP
+	_CUBA_CUDS_MAP = \
+		dict([(cls.cuba_key, cls) for name, cls \
+			in api_mod.__dict__.items() if isinstance(cls, type)])
