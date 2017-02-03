d9d8474c882b9e1345f1aa9ebe9be11d3f2fd836
Stefano Borini <sborini@enthought.com>
2017-02-03 18:09:39 +0000

    Fixed meta class generator base class

--- scripts/meta_class_generator.py
+++ scripts/meta_class_generator.py
@@ -2,32 +2,32 @@ from __future__ import print_function
 
 import os
 
+from simphony_metaparser.utils import traverse
+
 from .single_meta_class_generator import SingleMetaClassGenerator
 from . import utils
 
 
 class MetaClassGenerator(object):
-    def generate(self, simphony_metadata_dict, out_path):
+    def generate(self, ontology, output_dir):
         """
         Create the Simphony Metadata classes in the directory specified by
-        out_path, starting from the yaml-extracted data in
-        simphony_metadata_dict.
-
-        If the optional overwrite flag is True, the directory will be
-        emptied first.
+        output_dir, from the ontology elements.
         """
 
-        for key, _ in simphony_metadata_dict['CUDS_KEYS'].items():
-            gen = SingleMetaClassGenerator(key, simphony_metadata_dict)
+        for item, _ in traverse(ontology.root_cuds_item):
+            gen = SingleMetaClassGenerator()
 
             filename = os.path.join(
-                out_path,
-                "{}.py".format(utils.cuba_key_to_meta_class_module_name(key)))
+                output_dir,
+                "{}.py".format(utils.cuba_key_to_meta_class_module_name(
+                    item.name)))
 
             # Now write the code
             with open(filename, 'wb') as generated_file:
-                gen.generate(out=generated_file)
+                gen.generate(ontology, item, generated_file)
 
         # Create an empty __init__.py
-        init_path = os.path.join(out_path, '__init__.py')
-        open(init_path, 'a').close()
+        init_path = os.path.join(output_dir, '__init__.py')
+        with open(init_path, 'a'):
+            pass
