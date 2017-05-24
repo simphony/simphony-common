a52a1618e71e6c22ec57916dfda71c7e9a326c24
Stefano Borini <sborini@enthought.com>
2017-05-24 15:48:31 +0100

    Update numpy and tables

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ setup(
     install_requires=[
         "enum34>=1.0.4",
         "stevedore>=1.2.0",
-        "numpy>=1.4.1"],
+        "numpy>=1.12"],
     extras_require={
-        'H5IO': ["tables>=3.1.1"],
+        'H5IO': ["tables>=3.2.3.1"],
         'CUBAGen': ["click >= 3.3", "pyyaml >= 3.11"]},
     packages=find_packages(),
     entry_points={
