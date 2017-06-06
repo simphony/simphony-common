85f6b219021da7f06f8e52c40cb386f9439a4fa2
Stefano Borini <sborini@enthought.com>
2017-06-06 10:53:13 +0100

    Ported data from metadata repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import textwrap
 from subprocess import check_call, CalledProcessError
 
 from setuptools import setup, find_packages
-from distutils.cmd import Command
+from distutils.command.build import build
 
 # Read description
 with open('README.rst', 'r') as readme:
@@ -25,76 +25,24 @@ def cd(path):
         os.chdir(prev_cwd)
 
 
-class BuildMeta(Command):
-    user_options = [('repopath=', None,
-                     "Directory where to look for the local "
-                     "simphony-metadata repository clone"),
-                    ('repourl=', None, "URL to the github repo for cloning"),
-                    ('repotag=', None, "Tag to checkout before building")]
-
-    def initialize_options(self):
-        self.repopath = None
-        self.repourl = "https://github.com/simphony/simphony-metadata/"
-        self.repotag = None
-
-    def finalize_options(self):
-        if self.repopath is None:
-            self.repopath = os.path.join(os.getcwd(), "simphony-metadata/")
-
+class Build(build):
     def run(self):
-        try:
-            if not os.path.exists(self.repopath):
-                print("Checking out {}".format(self.repopath))
-                check_call(["git", "clone", self.repourl])
-        except OSError:
-            print("Failed to run git clone. "
-                  "Make sure it is installed in your environment")
-            raise
-        except CalledProcessError:
-            print("Failed to run git clone.")
-            raise
+        ontology_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "ontology")
 
-        if self.repotag is not None:
-            with cd(self.repopath):
-                try:
-                    print("Stashing possible changes.")
-                    check_call(["git", "stash"])
-                except CalledProcessError:
-                    print("Failed to run git stash.")
-                    raise
-
-                try:
-                    print("Fetching")
-                    check_call(["git", "fetch"])
-                except CalledProcessError:
-                    print("Failed to fetch")
-                    raise
-
-                try:
-                    print("Checking out {}".format(self.repotag))
-                    check_call(["git", "checkout", self.repotag])
-                except CalledProcessError:
-                    print("Failed to checkout {}".format(self.repotag))
-                    raise
-
-        yaml_dir = os.path.join(self.repopath, "yaml_files")
-
-        if not os.path.exists(yaml_dir):
-            print(textwrap.dedent("""
-                Cannot find simphony-metadata YAML dir files.
-                Please specify an appropriate path to the simphony-metadata
-                git repository in setup.cfg.
-                """))
+        if not os.path.exists(ontology_dir):
+            print("Cannot find ontology dir files in {}".format(ontology_dir))
             raise RuntimeError("Unrecoverable error.")
 
-        print("Building")
+        print("Building classes from ontology")
         cmd_args = ["simphony-meta-generate",
-                    yaml_dir,
+                    ontology_dir,
                     "simphony",
                     "--overwrite"]
         check_call(cmd_args)
 
-        print("Running yapf")
+        print("Running yapf to reformat in pep8 style")
         cmd_args = ["yapf", "--style", "pep8", "--in-place"]
         try:
             check_call(cmd_args + ["simphony/core/cuba.py"])
@@ -108,6 +56,7 @@ class BuildMeta(Command):
                 pip install yapf
                 """))
             raise
+        build.run(self)
 
 
 def write_version_py(filename=None):
@@ -144,6 +93,6 @@ setup(
         'CUBAGen': []},
     packages=find_packages(),
     cmdclass={
-        'build_meta': BuildMeta,
+        'build': Build,
     },
     )
