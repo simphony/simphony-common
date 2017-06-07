0d1083aa0fb6925c2c7a7b8e43f4b05cce5e1289
Stefano Borini <sborini@enthought.com>
2017-06-07 09:56:16 +0100

    Updated appveyor build command

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -4,11 +4,12 @@ call setenv /x64
 rem install python packages
 pip install -r requirements.txt
 pip install -r dev_requirements.txt
+pip install -r doc_requirements.txt
 
 rem install simphony-common 
-python setup.py develop
+python setup.py install 
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd examples/plugin
-python setup.py develop
+python setup.py install
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
