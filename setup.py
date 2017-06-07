a522c8e00559d3c0c23b3fd666a8871be27da840
Stefano Borini <sborini@enthought.com>
2017-06-07 09:39:55 +0100

    Fixed flake in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from subprocess import check_call
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
 
